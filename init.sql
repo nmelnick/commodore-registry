5a423d4b32f5bc0c5db723f83268451a7d67b1c0
Nicholas Melnick <nrmelnick@gmail.com>
2022-02-22 19:54:12 -0500

    Add a bunch of new models and variants

--- init.sql
+++ init.sql
@@ -11,10 +11,27 @@ CREATE INDEX machine_name ON machine (name);
 INSERT INTO machine
   (name, date_created, date_modified)
 VALUES
+  ('PET/CBM', NOW(), NOW()),
   ('CBM-II', NOW(), NOW()),
+  ('VIC-20', NOW(), NOW()),
+  ('MAX Machine', NOW(), NOW()),
   ('64', NOW(), NOW()),
+  ('64GS', NOW(), NOW()),
+  ('SX-64', NOW(), NOW()),
   ('128', NOW(), NOW()),
-  ('Amiga 4000', NOW(), NOW());
+  ('16', NOW(), NOW()),
+  ('Plus/4', NOW(), NOW()),
+  ('Amiga 500', NOW(), NOW()),
+  ('Amiga 600', NOW(), NOW()),
+  ('Amiga 1000', NOW(), NOW()),
+  ('Amiga 1200', NOW(), NOW()),
+  ('Amiga 1500', NOW(), NOW()),
+  ('Amiga 2000', NOW(), NOW()),
+  ('Amiga 2500', NOW(), NOW()),
+  ('Amiga 3000', NOW(), NOW()),
+  ('Amiga 4000', NOW(), NOW()),
+  ('Amiga CDTV', NOW(), NOW()),
+  ('Amiga CD32', NOW(), NOW());
 
 -- Corresponds to the model sticker (B128)
 CREATE TABLE model (
@@ -30,6 +47,8 @@ CREATE INDEX model_name ON model (name);
 INSERT INTO model
   (machine_id, name, date_created, date_modified)
 VALUES
+  ((SELECT machine_id FROM machine WHERE name = 'VIC-20'), 'VIC-20', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'MAX Machine'), 'MAX Machine', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'CBM-II'), 'B128', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'CBM-II'), 'P500', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'CBM-II'), 'B500', NOW(), NOW()),
@@ -42,12 +61,34 @@ VALUES
   ((SELECT machine_id FROM machine WHERE name = 'CBM-II'), '720', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = '64'), '64', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = '64'), '64C', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = '64'), 'Educator 64', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'SX-64'), 'SX-64', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = '64GS'), '64GS', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = '128'), '128', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = '128'), '128D', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = '128'), '128DCR', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = '16'), '16', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = '16'), '116', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Plus/4'), 'Plus/4', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 500'), 'A500', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 500'), 'A500+', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 600'), 'A600', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 600'), 'A600HD', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 1200'), 'A1200', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 1500'), 'A1500', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 2000'), 'A2000', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 2000'), 'A2000HD', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 2500'), 'A2500', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 2500'), 'A2500HD', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 3000'), 'A3000', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 3000'), 'A3000T', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 3000'), 'A3000UX', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'Amiga 4000'), 'A4000/040 Desktop', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'Amiga 4000'), 'A4000/030 Desktop', NOW(), NOW()),
   ((SELECT machine_id FROM machine WHERE name = 'Amiga 4000'), 'A4000T/040 Tower', NOW(), NOW()),
-  ((SELECT machine_id FROM machine WHERE name = 'Amiga 4000'), 'A4000T/060 Tower', NOW(), NOW());
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga 4000'), 'A4000T/060 Tower', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga CDTV'), 'CDTV', NOW(), NOW()),
+  ((SELECT machine_id FROM machine WHERE name = 'Amiga CD32'), 'CD32', NOW(), NOW());
 
 CREATE TABLE owner (
   owner_id serial primary key,
@@ -66,6 +107,7 @@ CREATE TABLE device (
   owner_id integer NOT NULL,
   prefix varchar(8),
   serial_number varchar(32),
+  source varchar(32),
   description text,
   date_created timestamp with time zone,
   date_modified timestamp with time zone
